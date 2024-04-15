ff3f87fcd1e5858fc0d8da7016b60f09ea17fa00
hxriif <zaidbnuarifc@gmail.com>
2024-04-15 11:03:29 +0530

    changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,19 +9,21 @@ const Navbar = () => {
 
 
     return (
-        <div className=' flex justify-between bg-black p-2 '>
-            <div className=' flex items-center w-auto'>
+        <div className='flex justify-between bg-black p-2'>
+            <div className='flex items-center w-auto'>
                 <img className='rounded-full w-14 h-14' src='rythm_logo.jpg'></img>
                 <h1 className=' text-2xl font-bold text-red-600 '>𝐑𝐲𝐭𝐡𝐦 𝐦𝐮𝐬𝐢𝐜 𝐩𝐥𝐚𝐭𝐟𝐨𝐫𝐦</h1>
             </div>
-            <div className=' items-center flex me-10 '>
-                <Music2Icon className='text-red-500 ' strokeWidth={3}/>
-                <button><ChevronRight className='text-red-500' strokeWidth={3}/></button>
+            <div className='items-center flex me-10'>
+                <Music2Icon className='text-red-500' strokeWidth={3} />
+                <button><ChevronRight className='text-red-500' strokeWidth={3} /></button>
             </div>
-            
+
         </div>
 
     );
 };
 
 export default Navbar;
+
+
