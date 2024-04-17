39f87161294e20126a0bc6497f1ba2cef48c9803
hxriif <zaidbnuarifc@gmail.com>
2024-04-17 10:17:32 +0530

    changes_cards

--- app/MusicView/Cards.jsx
+++ app/MusicView/Cards.jsx
@@ -16,13 +16,13 @@ function Cards() {
                     </div>
                     <div >
                         <button className='flex gap-3'>
-                            <PlayCircle/>
-                            <Volume1/>
-                            <Volume2/>
+                            <PlayCircle className='text-white hover:text-red-600'/>
+                            <Volume1 className='text-white hover:text-red-600'/>
+                            <Volume2 className='text-white hover:text-red-600'/>
                         </button>
                     </div>
                 </div>
-                <div className=' rounded-lg w-auto h-auto p-2 mt-2 flex items-center justify-around'>
+                {/* <div className=' rounded-lg w-auto h-auto p-2 mt-2 flex items-center justify-around'>
                     <div className='rounded-lg  h-[9rem] bg-black p-5 '>
                         <img className='w-20 h-20' src='rythm_logo.jpg'></img>
                     </div>
@@ -72,7 +72,7 @@ function Cards() {
                             <Volume2/>
                         </button>
                     </div>
-                </div>
+                </div> */}
             </div>
         </>
     )
