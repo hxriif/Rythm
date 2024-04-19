a9f01b7047e886b929cea10f3eb9ecdfe476c70d
hxriif <zaidbnuarifc@gmail.com>
2024-04-19 11:45:21 +0530

    routing_added

--- app/MusicView/MusicCards.jsx
+++ app/MusicView/MusicCards.jsx
@@ -1,23 +1,22 @@
 import { HeartIcon, PlayCircle, Volume1, Volume2 } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 function MusicCards() {
     return (
-       <div className='flex w-full p-2  justify-between gap-3 rounded-lg hover:bg-blue-500  text-white  mt-2'>
-        <div>
-            <img  className='w-11  h-11 rounded-full' src='rythm_logo.jpg'/>
+        <div className='flex w-full p-2  justify-between gap-3 rounded-lg hover:bg-blue-500  text-white  mt-2'>
+            <div>
+                <img className='w-11  h-11 rounded-full' src='rythm_logo.jpg' />
+            </div>
+            <div className='flex items-center'>
+                <p>enjoy the next 30 minutes of uninterrupted music listening</p>
+            </div>
+            <div className='flex items-center gap-3'>
+                <Link href="/SongView">
+                    <button><PlayCircle /></button>
+                </Link>
+            </div>
         </div>
-           <div className='flex items-center'>
-              <p>enjoy the next 30 minutes of uninterrupted music listening</p>
-           </div>
-           <div className='flex items-center gap-3'>
-                <button><PlayCircle/></button>
-                <button><Volume1/></button>
-                <button><Volume2/></button>
-                <button><HeartIcon/></button>
-                
-           </div>
-       </div>
     )
 }
 
