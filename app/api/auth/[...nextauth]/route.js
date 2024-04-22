f39265e99a87e14a8e4870c234e17a7b681fc96f
hxriif <zaidbnuarifc@gmail.com>
2024-04-22 16:05:19 +0530

    google_auth_added

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,20 @@
+import nextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+
+
+
+export const authOptions={
+    providers:[
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+     secret: process.env.SECRET,
+}
+
+
+
+const handler=nextAuth(authOptions)
+export {handler as GET , handler as POST}
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/app/FirstView/Navbar";
-import Navbar2 from "./MusicView/Navbar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
