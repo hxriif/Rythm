fdd4b7811518d77a13fd7c03da1a25427c4549f0
hxriif <zaidbnuarifc@gmail.com>
2024-04-22 16:05:42 +0530

    liked songs page

---
+++ app/LikedSongsView/Likedsongs.jsx
@@ -0,0 +1,47 @@
+import { ChevronLeft, PlayCircle } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react'
+import LikedsongsCards from './LikedsongsCards';
+
+
+function Likedsongs() {
+    return (
+        <div className='text-white h-full w-full  p-3 '>
+            <div className=' rounded-lg '>
+                <div className='flex gap-4 h-[13rem] p-2 border-b-2 border-cyan-300 '>
+                    <div className=' w-48 p-2'>
+                        <img className='w-full h-full rounded-lg' src='liked_songs.jpg' />
+                    </div>
+                    <div className=' p-2 flex flex-col items-start justify-center w-[500px]'>
+                        <p className='font-bold'>Playlists</p>
+                        <h1 className='font-extrabold ' style={{ fontSize: '4rem' }}>Liked songs</h1>
+                        <Link href='/MusicView'>
+                        <button><ChevronLeft/></button>
+                        </Link>
+                    </div>
+                </div>
+                <div className=' overflow-y-auto max-h-[470px]'>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                    <LikedsongsCards/>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Likedsongs;
