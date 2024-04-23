af269b07d794c216692bfd4c7eaba354a719afbe
hxriif <zaidbnuarifc@gmail.com>
2024-04-23 10:38:09 +0530

    changes

--- app/MusicView/Sidebar2.jsx
+++ app/MusicView/Sidebar2.jsx
@@ -5,14 +5,57 @@ import PlaylistView from './PlaylistView'
 
 function Sidebar2() {
   return (
-    <div className=' h-full w-full rounded-lg p-3 bg-black'>
-        <div className=' rounded-lg  flex flex-col items-center'>
-          <Link href="/AccountView">
-           <img className='w-36 rounded-full p-2' src='rythm_logo.jpg'/>
-           </Link>
+    <div className=' h-full w-full rounded-r-lg p-3 bg- flex flex-col gap-3 '>
+        <div className='flex justify-center mt-16'>
+          <h1 className='text-white font-extrabold' style={{ fontSize: '2rem' }}>Trending artists</h1>
         </div>
-        <div className='  mt-2 flex flex-col items-center h-[26rem]'>
-             <PlaylistView/>
+        <div className='flex gap-3 flex-col mt-2 text-white '>
+          <div className='flex items-center gap-3'>
+            <img className='w-14 h-14 rounded-full' src='rythm_logo.jpg'/>
+            <h1>David Podsiadlo</h1>
+          </div>
+          <div className='flex items-center gap-3'>
+            <img className='w-14 h-14 rounded-full' src='rythm_logo.jpg'/>
+            <h1>David Podsiadlo</h1>
+          </div>
+          <div className='flex items-center gap-3'>
+            <img className='w-14 h-14 rounded-full' src='rythm_logo.jpg'/>
+            <h1>David Podsiadlo</h1>
+          </div>
+          <div className='flex items-center gap-3'>
+            <img className='w-14 h-14 rounded-full' src='rythm_logo.jpg'/>
+            <h1>David Podsiadlo</h1>
+          </div>
+        </div>
+        <div className='text-white mt-3 gap-2 '>
+                <div>
+                  <h1 className='font-extrabold' style={{ fontSize: '2rem' }}>Genres</h1>
+                </div>
+                <div className='flex gap-3 mt-2 ms-3'>
+                <div className='rounded-lg border p-1' >
+                  <button>Hip Hop</button>
+                </div>
+                <div className='rounded-lg border p-1' >
+                  <button>House</button>
+                </div>
+                <div className='rounded-lg border p-1' >
+                  <button>Country</button>
+                </div>
+                </div>
+                <div className='flex gap-5 mt-2 ms-3'>
+                <div className='rounded-lg border p-1' >
+                  <button>Classic</button>
+                </div>
+                <div className='rounded-lg border p-1' >
+                  <button>Pop</button>
+                </div>
+                <div className='rounded-lg border p-1' >
+                  <button>Blues</button>
+                </div>
+                <div className='rounded-lg border p-1' >
+                  <button>Rock</button>
+                </div>
+                </div>
         </div>
     </div>
   )
