667810399d69bfc13bed54b5d615aa85d7b3ad1f
hxriif <zaidbnuarifc@gmail.com>
2024-04-23 10:37:58 +0530

    changes

--- app/MusicView/Sidebar.jsx
+++ app/MusicView/Sidebar.jsx
@@ -1,31 +1,30 @@
-import { HomeIcon, LibraryIcon,  PlusIcon } from 'lucide-react'
+import { HeartIcon, HomeIcon, LibraryIcon, LogOutIcon, PlusIcon, UserCircle } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
 function Sidebar() {
     return (
-        <div className='h-full w-full flex flex-col   rounded-lg gap-2 p-2 bg-black'>
-            <div className='flex items-center p-2 bg-[#0d0d0d] text-white rounded-lg '>
+        <div className='h-full w-full flex flex-col   rounded-l-lg gap-9 p-2 bg-slate-800'>
+            <div className='flex items-center p-2  text-white rounded-lg flex-col '>
                 <img className='h-10 w-10 rounded-full' src='rythm_logo.jpg' />
-                <h1 className=' text-x text-blue-500  '>𝐑𝐲𝐭𝐡𝐦 𝐦𝐮𝐬𝐢𝐜 𝐩𝐥𝐚𝐭𝐟𝐨𝐫𝐦</h1>
+                <h1 className=' text-x text-blue-500  '>𝐑𝐲𝐭𝐡𝐦 </h1>
             </div>
-            <div className='text-white bg-[#0f0f0f] flex flex-col rounded-lg p-2   gap-5'>
-            <Link href="/FirstView">
-                <button className='flex gap-3'><HomeIcon strokeWidth={3}/>Home</button>
-            </Link>
-                <button className='flex gap-3'><LibraryIcon strokeWidth={3} /> Library</button>
-                <button className='flex gap-3'><PlusIcon strokeWidth={3}/> create Playlists</button>
+            <div className='text-white  flex flex-col rounded-lg p-2   gap-6 h-full items-center'>
+                <Link href="/FirstView">
+                    <button className='flex gap-3 hover:text-blue-600'><HomeIcon strokeWidth={3} /></button>
+                </Link>
+                <button className='hover:text-blue-600'><LibraryIcon strokeWidth={3} /></button>
+                <button className='hover:text-blue-600'><PlusIcon strokeWidth={3} /> </button>
+                <button className='hover:text-blue-600'><UserCircle strokeWidth={3} /> </button>
+                <Link href="/LikedSongsView">
+                    <button className='hover:text-blue-600'><HeartIcon /></button>  
+                </Link>
             </div>
-            <div className='text-white bg-[#0e0d0d] rounded-lg h-full w-full p-2 flex flex-col items-center overflow-y-auto '>
-                <div className='sticky'>
-                    <h1>Top rated songs</h1>
-                </div>
-                <p className='mt-2'>sldjfldkfjldf</p>
-                <p>sldjfldkfjldf</p>
-                <p>sldjfldkfjldf</p>
-                <p>sldjfldkfjldf</p>
-                <p>sldjfldkfjldf</p>
-                <p>sldjfldkfjldf</p>
+            <div className='flex flex-col items-center  h-full gap-3 '>
+                <Link href="/AccountView">
+               <button> <img className='rounded-full h-14 w-14 mt-8' src='rythm_logo.jpg' /></button>
+               </Link>
+                <LogOutIcon />
             </div>
         </div>
     )
