294ada5f76513b408abf6b36e362da3a23702e84
hxriif <zaidbnuarifc@gmail.com>
2024-04-23 13:42:05 +0530

    modified

--- app/MusicView/Sidebar2.jsx
+++ app/MusicView/Sidebar2.jsx
@@ -9,51 +9,47 @@ function Sidebar2() {
         <div className='flex justify-center mt-16'>
           <h1 className='text-white font-extrabold' style={{ fontSize: '2rem' }}>Trending artists</h1>
         </div>
-        <div className='flex gap-3 flex-col mt-2 text-white '>
-          <div className='flex items-center gap-3'>
-            <img className='w-14 h-14 rounded-full' src='rythm_logo.jpg'/>
-            <h1>David Podsiadlo</h1>
+        <div className='flex gap-3 flex-col mt-2 font-extrabold text-yellow-300 '>
+          <div className='bg-cover bg-center w-full h-14 rounded-lg ' style={{backgroundImage: 'url("shallipopi.jpg")'}} >
+            <h1 className='text-right'>Shalli Poppi</h1>
           </div>
-          <div className='flex items-center gap-3'>
-            <img className='w-14 h-14 rounded-full' src='rythm_logo.jpg'/>
-            <h1>David Podsiadlo</h1>
+          <div className='bg-cover bg-center w-full h-14 rounded-lg ' style={{backgroundImage: 'url("big.jpg")'}} >
+            <h1 className='text-right'>Big Wizz</h1>
           </div>
-          <div className='flex items-center gap-3'>
-            <img className='w-14 h-14 rounded-full' src='rythm_logo.jpg'/>
-            <h1>David Podsiadlo</h1>
+          <div className='bg-cover bg-center w-full h-14 rounded-lg ' style={{backgroundImage: 'url("renee.jpg")'}} >
+            <h1 className='text-right'>renee rapp</h1>
           </div>
-          <div className='flex items-center gap-3'>
-            <img className='w-14 h-14 rounded-full' src='rythm_logo.jpg'/>
-            <h1>David Podsiadlo</h1>
+          <div className='bg-cover bg-center w-full h-14 rounded-lg ' style={{backgroundImage: 'url("brunoo.jpg")'}} >
+            <h1 className='text-right'>Bruno Mars</h1>
           </div>
         </div>
         <div className='text-white mt-3 gap-2 '>
                 <div>
-                  <h1 className='font-extrabold' style={{ fontSize: '2rem' }}>Genres</h1>
+                  <h1 className='font-extrabold ' style={{ fontSize: '2rem' }}>Category</h1>
                 </div>
-                <div className='flex gap-3 mt-2 ms-3'>
-                <div className='rounded-lg border p-1' >
-                  <button>Hip Hop</button>
+                <div className='flex gap-3 mt-2 ms-3 '>
+                <div className='rounded-full border border-opacity-40  bg-black   font-extrabold  ' >
+                  <button className='hover:text-slate-300 text-slate-500 p-1'>Hip Hop</button>
                 </div>
-                <div className='rounded-lg border p-1' >
-                  <button>House</button>
+                <div className='rounded-full border border-opacity-40  bg-black   font-extrabold ' >
+                  <button className='hover:text-slate-300 text-slate-500 p-1'>House</button>
                 </div>
-                <div className='rounded-lg border p-1' >
-                  <button>Country</button>
+                <div className='rounded-full border border-opacity-40  bg-black   font-extrabold ' >
+                  <button className='hover:text-slate-300 text-slate-500 p-1'>Country</button>
                 </div>
                 </div>
                 <div className='flex gap-5 mt-2 ms-3'>
-                <div className='rounded-lg border p-1' >
-                  <button>Classic</button>
+                <div className='rounded-full border border-opacity-40  bg-black   font-extrabold ' >
+                  <button className='hover:text-slate-300 text-slate-500 p-1'>Classic</button>
                 </div>
-                <div className='rounded-lg border p-1' >
-                  <button>Pop</button>
+                <div className='rounded-full border border-opacity-40  bg-black   font-extrabold ' >
+                  <button className='hover:text-slate-300 text-slate-500 p-1'>Pop</button>
                 </div>
-                <div className='rounded-lg border p-1' >
-                  <button>Blues</button>
+                <div className='rounded-full border border-opacity-40  bg-black   font-extrabold ' >
+                  <button className='hover:text-slate-300 text-slate-500 p-1'>Blues</button>
                 </div>
-                <div className='rounded-lg border p-1' >
-                  <button>Rock</button>
+                <div className='rounded-full border border-opacity-40  bg-black   font-extrabold ' >
+                  <button className='hover:text-slate-300 text-slate-500 p-1'>Rock</button>
                 </div>
                 </div>
         </div>
