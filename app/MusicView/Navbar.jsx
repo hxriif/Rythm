809583f906d49bbc324929ffe2b0a76091d71327
hxriif <zaidbnuarifc@gmail.com>
2024-04-23 10:37:45 +0530

    changes

--- app/MusicView/Navbar.jsx
+++ app/MusicView/Navbar.jsx
@@ -14,9 +14,6 @@ function Navbar2() {
     <div className="flex flex-col  h-full w-full rounded-lg " >
       <div className="flex gap-4 items-center  h-[5rem]  w-full  justify-center bg-black  rounded-lg">
         <input className='rounded-full p-1 h-8 w-72 text-black text-xs ' placeholder='search here ' />
-        <Link href="/LikedSongsView">
-        <button><HeartIcon/></button> 
-        </Link>
       </div>
       <div className='h-full  text-black w-full rounded-b-lg  bg-black overflow-y-auto '>
         <MusicCards/>
