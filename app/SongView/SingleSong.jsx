dc3d848993489eba192d5f2268c10c57485d555b
hxriif <zaidbnuarifc@gmail.com>
2024-04-23 16:03:29 +0530

    changes

--- app/SongView/SingleSong.jsx
+++ app/SongView/SingleSong.jsx
@@ -1,37 +1,23 @@
 import { HeartIcon, PlayCircle, Volume1, Volume2 } from 'lucide-react'
 import React from 'react'
+import Player from '../MusicView/Player'
 
 function SingleSong() {
   return (
-    <div className='text-black h-screen w-full flex justify-center p-2 '>
-      <div className=' w-full max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl h-auto flex flex-col p-2 rounded-lg bg-white'>
-        <div className='w-full h-auto flex flex-col md:flex-row gap-5 p-2'>
-          <img className='h-52 w-52 rounded-lg' src='rythm_logo.jpg' alt='Rythm Logo' />
-          <div className='w-full md:w-52 h-auto flex flex-col items-center justify-center'>
-            <p>aufhsdhfskdhf</p>
-            <p>aufhsdhfskdhf</p>
-            <p>aufhsdhfskdhf</p>
-            <p>aufhsdhfskdhf</p>
-            <p>aufhsdhfskdhf</p>
-            <p>aufhsdhfskdhf</p>
+    <div className=' h-screen w-full flex justify-center p-2 items-center '>
+      <div className=' w-[30rem] h-[29.8rem] rounded-lg  p-1 bg-slate-500'>
+        <div className='bg-black h-[300px] p-2 flex  justify-start items-center rounded-lg rounded-b-none gap-3'>
+          <div className='w-[14rem] h-[14rem] '>
+            <img className='w-full h-full rounded-lg' src='home_image.webp' />
+          </div>
+          <div className='w-[14rem] h-[14rem] rounded--lg  flex flex-col justify-center  '>
+            <p>song</p>
+            <p className='font-extrabold' style={{ fontSize: "2rem" }}>Mockingbrid</p>
+            <p>artist:<span>mockingbird</span></p>
           </div>
         </div>
-        <div className='mt-2 rounded-lg flex flex-col p-2'>
-          <p>EMinem</p>
-          <p>Mockingbird</p>
-          <button><HeartIcon /></button>
-        </div>
-        <div className='flex flex-col md:f justify-center items-center mt-4'>
-          <div className='flex gap-3'>
-            <button><Volume1 /></button>
-            <button><PlayCircle /></button>
-            <button><Volume2 /></button>
-          </div>
-          <div className='flex items-center'>
-            <p>0:01</p>
-            <p>_________________________________________________</p>
-            <p>3:25</p>
-          </div>
+        <div className='h-[166px]'>
+           <Player/>
         </div>
       </div>
     </div>
