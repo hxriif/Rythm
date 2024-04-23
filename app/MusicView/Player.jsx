6a804926d8bdc8d7e6126ce7629e8253cc37fb87
hxriif <zaidbnuarifc@gmail.com>
2024-04-23 16:03:05 +0530

    changes

--- app/MusicView/Player.jsx
+++ app/MusicView/Player.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
-import { PauseCircleIcon, PlayCircleIcon, Volume1, Volume2, ShuffleIcon, SkipBack, SkipForward, HeartPulse } from 'lucide-react';
+import { PauseCircleIcon, PlayCircleIcon, Volume1, Volume2, ShuffleIcon, SkipBack, SkipForward, HeartPulse, HeartIcon } from 'lucide-react';
 
 function Player() {
   return (
-    <div className='h-16 bg-black border-t border-neutral-700   text-xs md:text-base px-2 md:px-8 flex justify-between p-3  '>
-    <div className='flex p-2'>
-      <h1>dfiifg</h1>
+    <div className='w-full h-full text-white bg-black rounded-lg rounded-t-none'>
+       <div className='flex justify-center gap-4 p-2 '>
+        <button><ShuffleIcon className='w-5 h-5'/></button>
+         <button><SkipBack className='w-6 h-6'/></button>
+         <button><PlayCircleIcon className='w-9 h-9' /></button>
+         <button><SkipForward className='w-6 h-6'/></button>
+         <button><HeartIcon  className='w-5 h-5'/></button>
+       </div>
+       <div>
+
+       </div>
     </div>
-    <div>
-      <div className='flex bg-red-600  '>
-        <PlayCircleIcon  className='w-10 h-10' />
-        </div>
-        <div>
-          <h1>ifgiugfegf</h1>
-        </div>
-    </div>
-    <div className='flex  p-2 '>
-      <Volume1  />
-      <Volume2  />
-    </div>
-  </div>
   )
 }
 
