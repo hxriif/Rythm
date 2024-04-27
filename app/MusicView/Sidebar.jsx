79f933aff14b5a426b957353a19101c447a15f28
hxriif <zaidbnuarifc@gmail.com>
2024-04-27 16:17:43 +0530

    changes

--- app/MusicView/Sidebar.jsx
+++ app/MusicView/Sidebar.jsx
@@ -1,8 +1,17 @@
-import { HeartIcon, HomeIcon, LibraryIcon, LogOutIcon, PlusIcon, UserCircle } from 'lucide-react'
+"use client"
+
+import { CreativeCommonsIcon, HeartIcon, HomeIcon, LibraryIcon, LogOutIcon, PlusIcon, PlusSquareIcon, UserCircle } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 function Sidebar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+
     return (
         <div className='h-full w-full flex flex-col   rounded-l-lg gap-9 p-2 '>
             <div className='flex items-center p-2  text-white rounded-lg flex-col '>
@@ -16,19 +25,31 @@ function Sidebar() {
                     </Link>
                 </div>
                 <div>
-                    <button className='hover:text-blue-600'><LibraryIcon /></button>
-                </div>
-                <div>
-                    <button className='hover:text-blue-600'><PlusIcon /> </button>
-                </div>
-                <div>
-                    <button className='hover:text-blue-600'><UserCircle /> </button>
-                </div>
-                <div>
                     <Link href="/LikedSongsView">
                         <button className='hover:text-blue-600'><HeartIcon /></button>
                     </Link>
                 </div>
+                <div>
+                    <button className='hover:text-blue-600'><LibraryIcon /></button>
+                </div>
+                <div className="relative">
+                    <button onClick={toggleDropdown} className="flex items-center hover:text-blue-600 focus:outline-none">
+                        <PlusIcon />
+                    </button>
+                    {isOpen && (
+                        <div className="origin-top-right absolute left-0 mt-2 w-32 rounded-md shadow-lg  ring-1 ring-black ring-opacity-5">
+                            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                <Link href='/CreatePlaylist'>
+                                    <button className="block px-4 py-2 text-sm text-gray-400 hover:bg-gray-100 w-full text-left" role="menuitem">Create Playlist</button>
+                                </Link>
+                                <Link href="/UploadMusic">
+                                    <button className="block px-4 py-2 text-sm text-gray-400 hover:bg-gray-100 w-full text-left" role="menuitem">Upload music</button>
+                                </Link>
+                            </div>
+                        </div>
+                    )}
+                </div>
+
             </div>
             <div className='flex flex-col items-center  h-full gap-3 '>
                 <Link href="/AccountView">
--- app/MusicView/Sidebar2.jsx
+++ app/MusicView/Sidebar2.jsx
@@ -4,9 +4,9 @@ import PlaylistView from './PlaylistView';
 
 function Sidebar2() {
   return (
-    <div className='h-full w-full rounded-r-lg p-3 bg-flex flex-col gap-3'>
-      <div className='flex justify-center mt-16'>
-        <h1 className='text-white font-extrabold text-center lg:text-left' style={{ fontSize: '2rem' }}>Trending artists</h1>
+    <div className='h-full w-full rounded-r-lg p-3 bg-flex flex-col gap-3 '>
+      <div className='flex  mt-16'>
+        <h1 className='text-white font-extrabold text-center lg:text-left' style={{ fontSize: '1rem' }}>Trending artists</h1>
       </div>
       <div className='flex flex-col gap-3 mt-2 font-extrabold text-yellow-300'>
         <div className='bg-cover bg-center w-full h-14 rounded-lg' style={{backgroundImage: 'url("shallipopi.jpg")'}}>
@@ -22,36 +22,36 @@ function Sidebar2() {
           <h1 className='text-right'>Bruno Mars</h1>
         </div>
       </div>
-      <div className='text-white mt-3 gap-2'>
-        {/* <div>
+      {/* <div className='text-white mt-3 gap-2'>
+        <div>
           <h1 className='font-extrabold text-center lg:text-left' style={{ fontSize: '2rem' }}>Category</h1>
-        </div> */}
-        {/* <div className='flex flex-col gap-3 mt-2 lg:flex-row lg:ms-3'>
-          <div className='rounded-full border border-opacity-40 bg-black font-extrabold'>
-            <button className='hover:text-slate-300 text-slate-500 p-1'>Hip Hop</button>
+        </div>
+        <div className='flex flex-col gap-3 mt-2 lg:flex-row lg:ms-3'>
+          <div className='rounded-full  bg-black font-extrabold'>
+            <button className='hover:text-slate-300 text-slate-500 '>Hip Hop</button>
           </div>
-          <div className='rounded-full border border-opacity-40 bg-black font-extrabold'>
-            <button className='hover:text-slate-300 text-slate-500 p-1'>House</button>
+          <div className='rounded-full  bg-black font-extrabold'>
+            <button className='hover:text-slate-300 text-slate-500 '>House</button>
           </div>
-          <div className='rounded-full border border-opacity-40 bg-black font-extrabold'>
-            <button className='hover:text-slate-300 text-slate-500 p-1'>Country</button>
+          <div className='rounded-full  bg-black font-extrabold'>
+            <button className='hover:text-slate-300 text-slate-500 '>Country</button>
           </div>
         </div>
         <div className='flex flex-col gap-3 mt-2 lg:flex-row lg:ms-3'>
-          <div className='rounded-full border border-opacity-40 bg-black font-extrabold'>
-            <button className='hover:text-slate-300 text-slate-500 p-1'>Classic</button>
+          <div className='rounded-full  bg-black font-extrabold'>
+            <button className='hover:text-slate-300 text-slate-500 '>Classic</button>
           </div>
-          <div className='rounded-full border border-opacity-40 bg-black font-extrabold'>
-            <button className='hover:text-slate-300 text-slate-500 p-1'>Pop</button>
+          <div className='rounded-full  bg-black font-extrabold'>
+            <button className='hover:text-slate-300 text-slate-500 '>Pop</button>
           </div>
-          <div className='rounded-full border border-opacity-40 bg-black font-extrabold'>
-            <button className='hover:text-slate-300 text-slate-500 p-1'>Blues</button>
+          <div className='rounded-full  bg-black font-extrabold'>
+            <button className='hover:text-slate-300 text-slate-500 '>Blues</button>
           </div>
-          <div className='rounded-full border border-opacity-40 bg-black font-extrabold'>
-            <button className='hover:text-slate-300 text-slate-500 p-1'>Rock</button>
+          <div className='rounded-full  bg-black font-extrabold'>
+            <button className='hover:text-slate-300 text-slate-500 '>Rock</button>
           </div>
-        </div> */}
-      </div>
+        </div>
+      </div> */}
     </div>
   );
 }
