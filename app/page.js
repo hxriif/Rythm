5db5f310e35915ad713aeac2021279f36198dc9e
hxriif <zaidbnuarifc@gmail.com>
2024-04-27 16:16:41 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ import Navbar from "@/app/FirstView/Navbar";
 export default function Home() {
   return (
     <>
-        <div className="">
-          <div className="" >
+        <div>
+          <div>
           <Navbar/> 
           </div>
           <div className="overflow-y-auto max-h-[470px]" >
