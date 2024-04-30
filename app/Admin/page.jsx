945ca6a789e84190edfb6c8b5c2e3e04cbdd8a00
hxriif <zaidbnuarifc@gmail.com>
2024-04-30 13:15:29 +0530

    changes

--- app/Admin/page.jsx
+++ app/Admin/page.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
+import AdminPage from './AdminPage'
 
 function AdminHome() {
     return (
-        <>
-            <div className='bg-[#d7d2cc]  mt-2 ms-2 me-2 h-[33rem] rounded-lg p-2'>
-                <div className='flex gap-2 p-2  h-auto  justify-center '>
-                    <div className='flex gap-3 '>
-                    <img className='w-14 h-14 rounded-full' src='rythm_logo.jpg '></img>
-                    <h1 className=' text-2xl text-[#0a0504] '>𝐑𝐲𝐭𝐡𝐦 𝐦𝐮𝐬𝐢𝐜 𝐩𝐥𝐚𝐭𝐟𝐨𝐫𝐦</h1>
-                    </div>
-                </div>
-                <div className=' h-[27rem] text-black bg-white'>
-                    
-                </div>
-            </div>
-        </>
+       <div className='bg-slate-600 h-screen w-screen'>
+        <AdminPage/>
+       </div>
     )
 }
 
