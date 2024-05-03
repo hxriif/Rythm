a6259ceee9cb2c77dcdd95b9f6811117caa8e7b6
hxriif <zaidbnuarifc@gmail.com>
2024-05-03 16:36:20 +0530

    changes

---
+++ app/LoginPage/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './Login'
+
+function page() {
+  return (
+    <div className='h-screen w-screen'>
+      <Login/>
+    </div>
+  )
+}
+
+export default page
