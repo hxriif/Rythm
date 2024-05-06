76c5782e1736ceb4f586f1ed107efcedf67d0f3d
hxriif <zaidbnuarifc@gmail.com>
2024-05-06 12:17:34 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -14,8 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Toaster/>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<Toaster/></body>
     </html>
   );
 }
