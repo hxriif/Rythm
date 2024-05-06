1a10bf6baade40264bd8f084c4c937184d91a6f7
hxriif <zaidbnuarifc@gmail.com>
2024-05-06 12:17:02 +0530

    changes

--- app/CreatePlaylist/PlaylistCreat.jsx
+++ app/CreatePlaylist/PlaylistCreat.jsx
@@ -1,6 +1,43 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import Axios from '../axios'
+import toast from 'react-hot-toast'
+import { CircleChevronLeft } from 'lucide-react'
+import Link from 'next/link'
 
 function PlaylistCreat() {
+
+    const userId = localStorage.getItem("UserId")
+
+    const [Name, setName] = useState("")
+    const [Description, setdescription] = useState("")
+    const [Image, setImage] = useState(null)
+
+
+    const payload = { name: Name, description: Description, image: Image }
+
+    const playlistCreation = async () => {
+        try {
+            const response = await Axios.post(`/api/users/createPlaylist/${userId}`, payload)
+            if(response.status===200){
+            toast.success("Successfully created playlist")
+        }
+        } catch (error) {
+            console.log(error, 'failed')
+
+        }
+    }
+
+    useEffect(() => {
+        playlistCreation()
+    }, [])
+
+
+
+
+
+
     return (
         <div className='flex items-center justify-center  h-full w-full '>
             <div className='flex flex-col h-96 w-96 p-2 gap-3 justify-center items-center text-white rounded-lg bg-cover bg-center' style={{ backgroundImage: "url('bg.jpg')" }}>
@@ -8,16 +45,21 @@ function PlaylistCreat() {
                     <h1 className='font-extrabold text-cyan-500 ' style={{ fontSize: '2rem' }}>Create Your Playlist</h1>
                 </div>
                 <div>
-                    <input className='p-1  rounded-lg w-64 bg-black' type='text' placeholder='name' />
+                    <input className='p-1  rounded-lg w-64 bg-black' type='text' placeholder='name' onChange={(e) => setName(e.target.value)} />
                 </div>
                 <div>
-                    <input className='p-1  rounded-lg w-64 bg-black' type='text' placeholder='description' />
+                    <input className='p-1  rounded-lg w-64 bg-black' type='text' placeholder='description' onChange={(e) => setdescription(e.target.value)} />
                 </div>
                 <div>
-                    <input className='p-1  rounded-lg w-64 bg-black' type='file' placeholder='image' />
+                    <input className='p-1  rounded-lg w-64 bg-black' type='file' placeholder='image' onChange={(e) => setImage(e.target.value)} />
                 </div>
                 <div className='mt-8'>
-                    <button className='border rounded-lg p-2 hover:bg-slate-500'>Create</button>
+                    <button className='border rounded-lg p-2 hover:bg-slate-500' onClick={playlistCreation}>Create</button>
+                </div>
+                <div className='flex justify-end  w-full'>
+                    <Link href="/MusicHome">
+                    <button><CircleChevronLeft/></button>
+                    </Link>
                 </div>
             </div>
         </div>
