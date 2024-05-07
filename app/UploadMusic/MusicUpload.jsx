5325d3c131e89c4a665deef13a7708bd1798a19b
hxriif <zaidbnuarifc@gmail.com>
2024-05-07 11:16:06 +0530

    changes

--- app/UploadMusic/MusicUpload.jsx
+++ app/UploadMusic/MusicUpload.jsx
@@ -1,36 +1,71 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react';
+import Axios from '../axios';
+import toast from 'react-hot-toast';
+import Link from 'next/link';
+import { CircleChevronLeft } from 'lucide-react';
 
 function MusicUpload() {
+
+    const userId = typeof window !== 'undefined' ? localStorage.getItem("UserId") : null;
+
+
+    const [Name, setName] = useState("");
+    const [Image, setImage] = useState(null);
+    const [Category, setCategory] = useState("");
+    const [Description, setDescription] = useState("");
+    const [Artist, setArtist] = useState("");
+    const [Song, setSong] = useState("");
+
+    const payload = { name: Name, image: Image, category: Category, description: Description, artist: Artist, song: Song}
+
+
+    const handleUploadMusic = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('name', Name);
+        formData.append('image', Image);
+        formData.append('category', Category);
+        formData.append('artist', Artist);
+        formData.append('song', Song);
+
+
+        try {
+            const response = await Axios.post(`api/users/musicRequest/${userId}`, formData);
+            if (response.status === 200) {
+                toast.success("music added to pending request");
+                console.log("successs");
+            }
+        } catch (error) {
+            toast.error("error uploading music");
+        }
+    };
+
+
+
+
+
     return (
-        <div className='w-full h-full flex items-center justify-center '>
-            
-            <div className='h-[28rem] w-[28rem] flex flex-col items-center p-2 gap-2 bg-cover bg-center rounded-lg' style={{ backgroundImage: "url('bg.jpg')" }}>                <div className='mt-3 w-full flex justify-center p-2'>
-                <h1 className='font-extrabold text-cyan-500' style={{ fontSize: '20px' }}>Add Your Own Music To Rythm</h1>
-            </div>
-                <div className=''>
-                    <input className='p-1  rounded-lg w-64 bg-black' type='text' placeholder='name' />
-                </div>
-                <div>
-                    <input className='p-1 rounded-lg bg-black w-64 ' type='file' placeholder='image' />
-                </div>
-                <div>
-                    <input className='p-1 rounded-lg w-64 bg-black' type='text' placeholder='category' />
-                </div>
-                <div>
-                    <input className='p-1 rounded-lg w-64 bg-black' type='text' placeholder='description' />
-                </div>
-                <div>
-                    <input className='p-1 rounded-lg w-64 bg-black' type='text' placeholder='Artist' />
-                </div>
-                <div>
-                    <input className='p-1 rounded-lg w-64 bg-black' type='file' placeholder='song' />
-                </div>
-                <div className='mt-3'>
-                    <button className='bg-black text-white rounded-lg p-2 border hover:bg-slate-400'>Upload</button>
-                </div>
+        <div className='w-full h-full flex items-center justify-center'>
+            <div className='w-full md:w-[28rem] flex flex-col items-center p-2 gap-2 bg-cover bg-center rounded-lg' style={{ backgroundImage: "url('bg.jpg')" }}>
+            <form className='flex flex-col items-center text-black' onSubmit={handleUploadMusic}>
+                    <input className='mt-2 h-10 rounded-lg p-2' type='text' placeholder="Name" value={Name} onChange={(event) => setName(event.target.value)} />
+                    <input className='mt-2 h-10 rounded-lg p-2 bg-white w-56' type='file' placeholder="Image" onChange={(event) => setImage(event.target.files[0])} />
+                    <input className='mt-2 h-10 rounded-lg p-2' type='text' placeholder="category" value={Category} onChange={(event) => setCategory(event.target.value)} />
+                    <input className='mt-2 h-10 rounded-lg p-2' type='text' placeholder="description" value={Description} onChange={(event) => setDescription(event.target.value)} />
+                    <input className='mt-2 h-10 rounded-lg p-2' type='text' placeholder="artist" value={Artist} onChange={(event) => setArtist(event.target.value)} />
+                    <input className='mt-2 h-10 rounded-lg p-2' type='text' placeholder="song" value={Song} onChange={(event) => setSong(event.target.value)} />
+                    <div className='flex gap-2 ms-10 mt-4'> 
+                    <button className='h-10 text-black font-extrabold bg-slate-500 rounded-xl p-2 mt-2 hover:bg-slate-400' type='submit'>Upload</button>
+                    <Link href="/MusicHome">
+                    <button><CircleChevronLeft/></button>
+                    </Link>
+                    </div>
+                </form>
             </div>
         </div>
-    )
+    );
 }
 
-export default MusicUpload
+export default MusicUpload;
