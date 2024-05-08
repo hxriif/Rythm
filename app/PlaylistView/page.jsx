142ce3f024eb69e3234df6b96900c04c9d11cb37
hxriif <zaidbnuarifc@gmail.com>
2024-05-08 18:37:17 +0530

    changes

---
+++ app/PlaylistView/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Playlists from './Playlists'
+
+function Page() {
+  return (
+    <div className='h-screen w-screen'>
+        <Playlists/>
+    </div>
+  )
+}
+
+export default Page
